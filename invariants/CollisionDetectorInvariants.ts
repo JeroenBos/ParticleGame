c757fb8fa0ad320a543370c4b5d483adbed26048
JeroenBos <jer123bos@gmail.com>
2019-06-18 19:53:07 +0200

    Strengthen collision detector invariants

--- invariants/CollisionDetectorInvariants.ts
+++ invariants/CollisionDetectorInvariants.ts
@@ -19,9 +19,22 @@ class CollectionDetectorInvariants implements ICollectionDetector<P> {
         const result = this.obj.detect(particles);
         const { collisions, freeParticles } = result;
 
-        const collidedParticleIndices = new Set(collisions.map(collision => [collision.i, collision.j]).reduce((a, b) => a.concat(b)));
-        if (collidedParticleIndices.size + freeParticles.length != particles.length)
-            throw new Error('invariant failed');
+        const collidedParticleIndices = new Set(collisions.map(collision => [collision.i, collision.j]).reduce((a, b) => a.concat(b), []));
+        const freeParticleIndices = freeParticles.map(p => particles.indexOf(p));
+        for (const freeParticleIndex of freeParticleIndices) {
+            if (freeParticleIndex == -1) {
+                throw new Error('invariant failed: free particles returned from the collision detector must be chosen from the initial particles collection');
+            }
+        }
+
+        const returnedParticleIndices = new Set(freeParticleIndices.concat(Array.from(collidedParticleIndices)));
+        if (returnedParticleIndices.size != particles.length) {
+            if (returnedParticleIndices.size > particles.length)
+                throw new Error('Invariant failed: nonexistent particles were returned');
+            throw new Error(`Invariant failed: Not all particles either collided or didn't collide`);
+        }
+
+
 
         return result;
     }
