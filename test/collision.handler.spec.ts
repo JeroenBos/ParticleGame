5527dc5e91a4462bf8aa734ee907216975eb46be
JeroenBos <jer123bos@gmail.com>
2019-06-20 14:34:11 +0200

    Implement some collision handler tests

--- test/collision.handler.spec.ts
+++ test/collision.handler.spec.ts
@@ -45,4 +45,81 @@ describe('CollisionHandler', () => {
         assert(resultants[0].size == p.size);
         assert(resultants[1].size == q.size);
     });
+
+    it('Stationary overlapping particles along y-axis are placed adjacent', () => {
+        // arrange
+        const p: ParticleProps = {
+            x: 0,
+            y: 0,
+            vx: 0,
+            vy: 0,
+            size: 1,
+        };
+        const q: ParticleProps = {
+            x: 0,
+            y: 1,
+            vx: 0,
+            vy: 0,
+            size: 1,
+        };
+
+        // act
+        // debugger;
+        const resultants = collisionDetector.collide(p, q);
+
+        // assert
+        assert(resultants.length == 2);
+
+        assert(resultants[0].y == 1.5);
+        assert(resultants[1].y == -0.5);
+
+        // also test the trivial properties:
+        assert(resultants[0].vx == 0);
+        assert(resultants[0].vy == 0);
+        assert(resultants[1].vx == 0);
+        assert(resultants[1].vy == 0);
+        assert(resultants[0].x == 0);
+        assert(resultants[0].x == 0);
+        assert(resultants[0].size == p.size);
+        assert(resultants[1].size == q.size);
+    });
+
+
+    it('Stationary overlapping particles mirrored along y-axis are placed adjacent', () => {
+        // arrange
+        const p: ParticleProps = {
+            x: 0,
+            y: 1,
+            vx: 0,
+            vy: 0,
+            size: 1,
+        };
+        const q: ParticleProps = {
+            x: 0,
+            y: 0,
+            vx: 0,
+            vy: 0,
+            size: 1,
+        };
+
+        // act
+        // debugger;
+        const resultants = collisionDetector.collide(p, q);
+
+        // assert
+        assert(resultants.length == 2);
+
+        assert(resultants[0].y == -0.5);
+        assert(resultants[1].y == 1.5);
+
+        // also test the trivial properties:
+        assert(resultants[0].vx == 0);
+        assert(resultants[0].vy == 0);
+        assert(resultants[1].vx == 0);
+        assert(resultants[1].vy == 0);
+        assert(resultants[0].x == 0);
+        assert(resultants[0].x == 0);
+        assert(resultants[0].size == p.size);
+        assert(resultants[1].size == q.size);
+    });
 });
