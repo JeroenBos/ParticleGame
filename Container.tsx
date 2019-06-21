4a55a222d0879f5ea98ea866ec12bd8788c24bf2
JeroenBos <jer123bos@gmail.com>
2019-06-22 01:12:55 +0200

    Add and display timestamp

--- Container.tsx
+++ Container.tsx
@@ -15,6 +15,7 @@ export interface ContainerProps {
 
 export interface ContainerState {
     particles: ParticleProps[];
+    t: number;
 }
 
 export class Container extends React.Component<ContainerProps, ContainerState> {
@@ -23,7 +24,7 @@ export class Container extends React.Component<ContainerProps, ContainerState> {
         super(props);
         const initialParticles = props.particleGenerator.generate();
         const particles = this.props.engine.resolveInitialCollisions(initialParticles);
-        this.state = { particles };
+        this.state = { particles, t: 0 };
 
         setInterval(() => this.toTimestep(), props.updateInterval);
     }
@@ -31,6 +32,7 @@ export class Container extends React.Component<ContainerProps, ContainerState> {
         const particles = this.state.particles.map((p, i) => <Particle key={i} {...p}></Particle>);
         return (
             <div>
+                <div>t: {this.state.t}</div>
                 <svg width={this.props.width} height={this.props.height} >
                     {particles}
                     <rect x="0" y="0" width={this.props.width} height={this.props.height} fill="none" stroke="black"></rect>
@@ -42,7 +44,7 @@ export class Container extends React.Component<ContainerProps, ContainerState> {
     toTimestep(): void {
         this.setState(state => {
             const particles = this.props.engine.evolve(state.particles);
-            return { particles };
+            return { particles, t: state.t + 1 };
         });
     }
 }
