c42dceac5fd7a7ad96597c7e8b5b7d4315158016
JeroenBos <jer123bos@gmail.com>
2019-07-05 13:15:51 +0200

    Test counting to pi

--- scenarios/countToπ.ts
+++ scenarios/countToπ.ts
@@ -14,38 +14,27 @@ class ParticleGenerator implements IParticleGenerator<Particle> {
 }
 
 class Config extends DefaultConfig {
-    constructor(private readonly mass: number) {
+    constructor(private readonly params: { mass: number } & Partial<{initial_τ_per_s: number}>) {
         super()
     }
     createGenerator() {
-        const m = this.mass;
+        const m = this.params.mass;
         return new ParticleGenerator(m);
     }
     createGeometry() {
         return new BoxGeometry(Infinity, this.height);
     }
-
-    // get dt_ms() {
-    //     return this.updateInterval_ms / this.stepsPerTimeInterval;
-    // }
-
-    // get updateInterval_ms() {
-    //     return 10;
-    // }
-    // get stepsPerTimeInterval() {
-    //     return Math.max(1, this.mass * 10 ** tradeoff);
-    // }
 }
 
 export default {
-    precision0: () => new Config(10 ** 0),
-    precision1: () => new Config(10 ** 1),
-    precision2: () => new Config(10 ** 2),
-    precision3: () => new Config(10 ** 3),
-    precision4: () => new Config(10 ** 4),
-    precision5: () => new Config(10 ** 5),
-    precision6: () => new Config(10 ** 6),
-    precision7: () => new Config(10 ** 7),
-    precision_m16: () => new Config(16),
-    precision_m64: () => new Config(64)
+    precision0: () => new Config({ mass: 10 ** 0 }),
+    precision1: () => new Config({ mass: 10 ** 1 }),
+    precision2: () => new Config({ mass: 10 ** 2 }),
+    precision3: () => new Config({ mass: 10 ** 3 }),
+    precision4: () => new Config({ mass: 10 ** 4 }),
+    precision5: () => new Config({ mass: 10 ** 5 }),
+    precision6: () => new Config({ mass: 10 ** 6 }),
+    precision7: () => new Config({ mass: 10 ** 7 }),
+    precision_m16: () => new Config({ mass: 16 }),
+    precision_m64: () => new Config({ mass: 64 })
 };
--- test/countToπ.spec.ts
+++ test/countToπ.spec.ts
@@ -9,7 +9,7 @@ describe('countToπ', () => {
     it('m=16', () => {
         // arrange
         const config = configs.precision_m16();
-        config.dτ = 0.1;
+        config.dτ = 0.1; // optimization
         config.τ_max = 100;
         const originalParticles = config.particleGenerator.generate();
 
@@ -24,7 +24,6 @@ describe('countToπ', () => {
     it('m=64', () => {
         // arrange
         const config = configs.precision_m64();
-        config.dτ = 0.01;
         config.τ_max = 600;
         const originalParticles = config.particleGenerator.generate();
 
@@ -51,6 +50,53 @@ describe('countToπ', () => {
         const count = config.collisionDetector.count + config.geometry.bounces;
         assert(count == 3);
     });
+    
+    it('m=10', () => {
+        // arrange
+        const config = configs.precision1();
+        config.dτ = 0.1; // optimization
+        config.τ_max = 200;
+        const originalParticles = config.particleGenerator.generate();
 
+        // act
+        debugger;
+        config.engine.evolve(originalParticles, config.τ_max);
+
+        // assert
+        const count = config.collisionDetector.count + config.geometry.bounces;
+        assert(count == 10);
+    });
+
+    it('m=100', () => {
+        // arrange
+        const config = configs.precision2();
+        config.τ_max = 400;
+        const originalParticles = config.particleGenerator.generate();
+
+        // act
+        debugger;
+        config.engine.evolve(originalParticles, config.τ_max);
+
+        // assert
+        const count = config.collisionDetector.count + config.geometry.bounces;
+        assert(count == 31);
+    });
+
+    
+    it('m=10000', () => {
+        // arrange
+        const config = configs.precision4();
+        config.dτ = 0.002; // requires extra precision
+        config.τ_max = 400;
+        const originalParticles = config.particleGenerator.generate();
+
+        // act
+        debugger;
+        config.engine.evolve(originalParticles, config.τ_max);
+
+        // assert
+        const count = config.collisionDetector.count + config.geometry.bounces;
+        assert(count == 314);
+    });
 });
 
