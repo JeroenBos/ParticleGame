1f3783a0540c79203c999e71d797a47340577e16
JeroenBos <jer123bos@gmail.com>
2019-07-07 17:41:58 +0200

    Log time in html test

--- test/html.spec.ts
+++ test/html.spec.ts
@@ -14,6 +14,7 @@ const τ_max = Math.min(config.τ_max, 100000 * engine.dτ);
 describe('HTML', () => {
     it('The same as diplayed in the html', () => {
         // arrange
+        const initial_τ_per_s = 10;
         const stepCount = τ_max / engine.dτ;
         const generatedParticles = particleGenerator.generate();
         const initialParticles = engine.resolveInitialCollisions(generatedParticles);
@@ -24,6 +25,7 @@ describe('HTML', () => {
         for (let i = 0; i < stepCount; i++) {
             const particlesBefore = particles;
             particles = engine.evolve(particles, engine.dτ);
+            console.log('t: ' + (i * engine.dτ / initial_τ_per_s).toFixed(1));
             // console.log(`${i}. p1: {${particles[0].x}, ${particles[0].vx}}. p1: {${particles[1].x}, ${particles[1].vy}}, imparted: ${geometry.impartedMomentum.px}, collisions: ${collisionDetector.count}`);
 
             if (config.forceComputer instanceof ZeroForce) {
