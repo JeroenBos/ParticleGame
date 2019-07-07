5b875ea88084eb39b1f5907a66421bc5b642a8ea
JeroenBos <jer123bos@gmail.com>
2019-07-07 13:00:10 +0200

    Test import of seedrandom

--- test/index.ts
+++ test/index.ts
@@ -6,3 +6,12 @@ import './geometry.spec';
 import './forceComputer.spec';
 import './countToπ.spec';
 import './html.spec';
+
+
+import * as seedrandom from 'seedrandom';
+describe('seedrandom', () => {
+    it('import succeeded', () => {
+        const x = seedrandom('0')();
+        if (!(0 < x && x < 1)) throw new Error();
+    });
+});
